d8d36714e2dc729cc7ad98ec715607936d7dbefc
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-10 14:11:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function secondHighest(){
+	let highest = -Infinity;
+	let secondHighest = -Infinity;
+
+	for(let i = 0; i<arr.length; i++) {
+		if (arr[i] > highest) {
+			secondHighest = highest;
+			highest = arr[i];
+			
+		}else if (arr[i]> secondHighest && arr[i] !== highest){
+			secondHighest = arr[i];
+			
+		}
+	}
+
+	return secondHighest;
+}
